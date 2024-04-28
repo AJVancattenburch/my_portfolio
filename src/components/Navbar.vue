8ab153bd7d319b462f4b3814c510e62532a41d72
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-04-28 11:29:47 -0600

    reverting back to top nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,14 @@
 
 <template>
-  <nav class="navbar navbar-expand-sm custom-navbar black-gradient px-3">
-    <router-link class="col-3 navbar-brand d-flex" :to="{ name: 'Home' }">
+    <nav class="">
+      <ul v-for="link in navLinks" :key="link.id" class="nav-list">
+        <li :disabled="!link.isActive" class="active">
+          <a class="nav-link" @click="scrollTo(link?.id)" aria-current="page">{{ link.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  <!-- <nav class="">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex align-items-center">
         <img alt="logo" src="../assets/img/icons/logo.svg" class="my-logo" height="45" />
         <span class="my-name">AJ Vancattenburch</span>
@@ -11,40 +18,48 @@
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="col-9 collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="nav-list">
+        <li class="active">
           <router-link class="nav-link" :to="{ name: 'Resume' }">My Resume</router-link>
         </li>
         <li v-for="link in navLinks" :key="link.id" class="nav-item">
           <span v-if="link.primary" @click="scrollTo(link?.id)" :title="link.title" class="nav-link">{{ link.name }}</span>
         </li>
       </ul>
-      <!-- LOGIN COMPONENT HERE -->
+      LOGIN COMPONENT HERE
       <div>
         
       </div>
     </div>
-  </nav>
+  </nav> -->
 </template>
 
 <script>
+import { ref } from "vue";
 import navLinks from '../constants/NavLinks';
 import { logger } from "../utils/Logger.js";
 
 export default {
   setup() {
-    //Scrolls to elements and compensates for fixed headers, so that the header does not cover the element's heading. Adjust padding (the `yOffset` constant below) as needed. 1 increment = 1px.
+
+    const isActive = ref()
+
     function scrollTo(id) {
       const yOffset = -65;
       let scrollElem = document.getElementById(id);
       logger.log(`Scrolling to ${id}. Found element: ${scrollElem}.`);
+     
       if (!scrollElem) {
-    logger.error(`Element with id ${id} not found in navLinks.`);
-    return;
-  }
-  let y = scrollElem.getBoundingClientRect().top + window.scrollY + yOffset;
-  window.scrollTo({top: y, behavior: 'smooth'})
+        logger.error(`Element with id ${id} not found.`);
+        return;
+      }
+      adjustScrollYOffset(scrollElem, yOffset);
+    }
+
+    function adjustScrollYOffset(scrollElem, yOffset) {
+      let y = scrollElem.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({top: y, behavior: 'smooth'})
     }
     return {
       scrollTo,
@@ -62,24 +77,95 @@ export default {
   font-family: 'Poppins', sans-serif;
   padding: 0;
   margin: 0;
+  box-sizing: border-box;
+  font-size: 1.25rem;
+  background: var(--black-purple-gradient);
 }
 
-.custom-navbar {
+nav {
+  padding-inline-end: 20rem;
   position: fixed;
   top: 0;
+  bottom: 0;
   left: 0;
-  right: 0;
-  width: 100%;
+  background: var(--black-purple-radial-gradient);
+  padding-block: 2rem;
+  width: 0;
+  max-width: 10rem;
+  height: 100vh;
   z-index: 1000;
-  filter: brightness(0.9);
-  .my-logo {
-    border-radius: 50%;
-  }
-  .my-name {
-    font-weight: 550;
-    margin-left: 1rem;
-    text-shadow: 0 1px 2px var(--blue);
-  }
+}
+
+.nav-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+  display: grid;
+  gap: 1rem;
+}
+
+.nav-list .nav-link {
+  color: white;
+  display: block;
+  text-decoration: none;
+}
+
+.nav-list li {
+  padding-block: 1rem;
+  padding-inline: 2rem;
+  margin-inline: 1rem 0;
+  /* prevent animation glitch on right side */
+  box-shadow: 2px 0 0 var(--black-purple-gradient);
+}
+
+.nav-list li.active {
+  view-transition-name: nav;
+  display: block;
+  background: var(--black-purple-gradient);
+  position: relative;
+  z-index: -1;
+  border-radius: 100vw 0 0 100vw;
+}
+
+::view-transition-group(nav) {
+  animation-duration: 1s;
+}
+
+.nav-list li.active::before,
+.nav-list li.active::after {
+  --border-radius: 1rem;
+
+  content: "";
+  position: absolute;
+  width: var(--border-radius);
+  height: var(--border-radius);
+  right: 0rem;
+  background: var(--black-purple-radial-gradient);
+}
+
+.nav-list li.active::before {
+  border-radius: 0 0 var(--border-radius);
+  top: calc(var(--border-radius) * -1);
+  box-shadow: 5px 5px 0 5px var(--black-purple-gradient);
+}
+
+.nav-list li.active::after {
+  border-radius: 0 var(--border-radius) 0 0;
+  bottom: calc(var(--border-radius) * -1);
+  box-shadow: 5px -5px 0 5px var(--black-purple-gradient);
+}
+
+.nav-list li:nth-child(1) a {
+  view-transition-name: test;
+}
+
+.nav-list li:nth-child(2) a {
+  view-transition-name: test-2;
+}
+
+.nav-list li:nth-child(3) a {
+  view-transition-name: test-3;
 }
 .navbar-toggler {
   padding: 0.5rem;
@@ -95,31 +181,9 @@ export default {
   }
 }
 
-
-
-a:hover {
-  text-decoration: none;
-}
-
-.navbar-nav .nav-item {
-  display: flex;
-  align-items: center;
-  margin: 0 0.5rem;
-
-  .nav-link {
-    text-transform: uppercase;
-  }
-}
-
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
 @media screen and (min-width: 576px) {
   nav {
-    height: 64px;
+    height: auto;
   }
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,26 @@
 <template>
-  <Hero />
-  <div class="bg-main">
-    <About />
-    <Education />
-    <Experience />
-    <Technologies />
-    <Projects />
-    <Feedback />
-    <Contact />
+  <div class="col-12 d-flex">
+    <div class="col-4 col-lg-3 d-flex flex-column align-items-start">
+      <NavBar />
+    </div>
+    <div class="col-8 col-lg-9 bg-main d-flex flex-column justify-content-center align-items-start">
+      <Hero />
+        <About />
+        <Education />
+        <Experience />
+        <Technologies />
+        <Projects />
+        <Feedback />
+        <Contact />
+        <footer class="black-gradient text-light px-3">
+          Designed By: Anthony Vancattenburch
+        </footer>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/Navbar.vue';
 import Hero from '../components/Hero.vue';
 import About from '../components/About.vue';
 import Education from '../components/Education.vue';
@@ -23,6 +32,7 @@ import Contact from '../components/Contact.vue';
 
 export default {
   components: {
+    NavBar,
     Hero,
     About,
     Education,
@@ -40,6 +50,14 @@ export default {
   position: relative;
   background: var(--black-purple-radial-gradient);
   height: 100%;
+  width: 100%;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  display: grid;
+  height: 32px;
   width: 100%;
 }
 </style>
