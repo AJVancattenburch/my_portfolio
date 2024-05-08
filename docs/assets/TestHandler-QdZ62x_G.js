b88f4c364362de5007bd4c62de8e9e873c07493c
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-05-08 17:10:06 -0600

    add docs

---
+++ docs/assets/TestHandler-QdZ62x_G.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-njByACJb.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Received IS_TESTED",e)}}const r=new t;export{r as testHandler};
