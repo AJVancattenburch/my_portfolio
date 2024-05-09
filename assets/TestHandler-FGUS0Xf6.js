1af8753b8223f286bc3b62cf3078c38d4096c224
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-05-09 17:53:47 -0600

    safari fix for gradient text

--- assets/TestHandler-FGUS0Xf6.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-mzSsOWxx.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Received IS_TESTED",e)}}const r=new t;export{r as testHandler};
