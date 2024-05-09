5a286dff4c96805313418d45bc5c5af0e7c66779
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-05-08 19:23:42 -0600

    dropping repo name from portfolio website

--- assets/TestHandler-CqhEEzDE.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-zRtJ4MAg.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Received IS_TESTED",e)}}const r=new t;export{r as testHandler};
