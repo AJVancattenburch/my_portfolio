e663554f92aa9ca21800995463bb0ff57252ff08
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-05-08 18:18:23 -0600

    add docs

---
+++ assets/TestHandler-SLHvilG8.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index-2XJ9ys0P.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Received IS_TESTED",e)}}const r=new t;export{r as testHandler};
