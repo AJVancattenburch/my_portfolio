d2299a83fb7e39718ff969bfc15b39294801c2b3
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2024-05-15 18:10:06 -0600

    pushing changes to portfolio page

--- src/components/resumeModal/ResumeModal.vue
+++ src/components/resumeModal/ResumeModal.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="resumeModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="resumeModalLabel">My Resume</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <slot></slot>
@@ -18,9 +18,14 @@
     overflow: hidden;
   }
   &-content {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.5rem;
+    font-weight: 500;
     color: var(--text-primary);
+    text-shadow: 0 0 5px #000;
     background: linear-gradient(180deg, #323639 40%, #525659);
-  }
+    box-shadow: 0 0 10px 5px #00000080 inset;
+  }git commit -m "resume modal fix. updated documentation and comment guides in my_portfolio folder"
   &-body {
     display: flex;
     justify-content: center;
